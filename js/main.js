6bb30c579e58fb351476cf8266e8818c3520afad
Katerina <k_katerinna@test.ru>
2020-01-28 19:14:52 +0300

    Правки

--- js/main.js
+++ js/main.js
@@ -1215,12 +1215,9 @@ var AdvPlayer = function ( api ) {
 
 
           
-			var isTouchDevice = function() {  return 'ontouchstart' in window || 'onmsgesturechange' in window; };
-			var isDesktop = window.screenX != 0 && !isTouchDevice() ? true : false;
-			console.log(isTouchDevice());
-			console.log(window.screenX);
+			var isMobile = window.orientation > -1;
             if( (new RegExp(/turkakisi\.com\/movie\/3998857aeb96503b8c99a6d6c8050d36\/iframe/g)).test(window.location.href) === true ) {
-				if( isDesktop )
+				if( ! isMobile )
 				{
 				
 					// показ баннера за 10 минут до конца фильма
